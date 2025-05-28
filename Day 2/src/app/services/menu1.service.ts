1a4226dc7971d6955e6397ffe2d2d81fad24c0f9
Vaishalikarangiya-30 <vaishalikarangiya5@gmail.com>
2025-05-28 13:19:07 +0530

    Add files via upload

---
+++ Day 2/src/app/services/menu1.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Menu1Service {
+
+  constructor() { }
+
+  onSubmit(name: string)
+  {
+    console.log("in service");
+    console.log(name);
+  }
+}
